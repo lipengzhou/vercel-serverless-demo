5383cea90d9e0db25e23dab337f4e6172c509e0a
lipengzhou <lpzmail@163.com>
2020-11-24 21:04:00 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,31 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>数据</h2>
+    <ul>
+      <li v-for="item in list" :key="item.id">{{ item.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      list: []
+    }
+  },
+  async created () {
+    const { data } = await axios.get('/api/front/ad/getAdList')
+    this.list = data.content
   }
 }
 </script>
