a18fd13a0cede82a7ea09954eda6b405c7eb62cf
lipengzhou <lpzmail@163.com>
2020-11-24 21:36:04 +0800

    update

--- api/front.js
+++ api/front.js
@@ -1,13 +1,13 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
-module.exports = (req, res) => {
-  res.send('front => ' + req.url)
-}
+// module.exports = (req, res) => {
+//   res.send('front => ' + req.url)
+// }
 
-// module.exports = createProxyMiddleware({
-//   target: 'http://edufront.lagou.com/front/',
-//   pathRewrite: {
-//     '^/api/front': ''
-//   },
-//   changeOrigin: true
-// })
+module.exports = createProxyMiddleware({
+  target: 'http://edufront.lagou.com/front/',
+  pathRewrite: {
+    '^/api/front': ''
+  },
+  changeOrigin: true
+})
