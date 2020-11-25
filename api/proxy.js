c582752d01974a1fd2367ff388519222ec5be1bb
lipengzhou <lpzmail@163.com>
2020-11-25 11:02:18 +0800

    update

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,17 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-  console.log(req.url)
-  res.json({
-    url: req.url
-  })
-  // let target = ''
-  // if (req.url.includes('/api/front')) {
-  //   target = 'http://edufront.lagou.com/'
-  // } else if (req.url.startsWith('/api/boss')) {
-  //   target = 'http://eduboss.lagou.com/'
-  // }
-  // createProxyMiddleware({
-  //   target,
-  //   changeOrigin: true,
-  //   pathRewrite: {
-  //     '^/api/': ''
-  //   }
-  // })(req, res)
+  let target = ''
+  if (req.url.includes('/api/front')) {
+    target = 'http://edufront.lagou.com/'
+  } else if (req.url.startsWith('/api/boss')) {
+    target = 'http://eduboss.lagou.com/'
+  }
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/': ''
+    }
+  })(req, res)
 }
